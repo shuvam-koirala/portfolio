0a7daadc28fbb696bcdb02557bd85bf7dc1bbef1
curious coder <suvamkoirala08@gmail.com>
2020-10-16 09:40:07 +0545

    Updates

---
+++ precache-manifest.628c73f9d5924cd4e3612006948c736e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43c50500800393506618731d5d7f52ca",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "8e2a15460d7787bd722f",
+    "url": "/portfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d9098bdcb696924f6b03",
+    "url": "/portfolio/static/js/2.2e5af390.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.2e5af390.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e2a15460d7787bd722f",
+    "url": "/portfolio/static/js/main.2cf26139.chunk.js"
+  },
+  {
+    "revision": "8494288b3d6c293af1c0",
+    "url": "/portfolio/static/js/runtime-main.1e873255.js"
+  }
+]);
